2e3e94729dcd6150ab1e3abdef5b5cbbe3771398
Warun S <warunsinx@gmail.com>
2023-02-05 22:43:17 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   images: { loader: "akamai", path: "" },
   trailingSlash: true,
+  assetPrefix: isProd ? "https://cretodus.0xwa.run" : "",
 };
 
 module.exports = nextConfig;
